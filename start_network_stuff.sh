e53461221fbff5cfa7500ac8da9da6b62b034c45
Richard G. Clegg <richard@richardclegg.org>
2014-08-15 15:21:01 +0100

    edit

--- start_network_stuff.sh
+++ start_network_stuff.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+/usr/sbin/openvpn --config /etc/openvpn/UCL-ovpn-client.conf --dev-type tap --dev tap0 &
+/usr/sbin/openvpn --config /etc/openvpn/ofelia.conf --dev-type tun --dev tun0 &
+
+#set up vlans
 /sbin/modprobe 8021q
-/usr/sbin/openvpn --config /etc/openvpn/UCL-ovpn-client.conf 12> /home/connet/UCLvpn.log &
 /sbin/vconfig add eth4 10
 /sbin/vconfig add eth4 11
 /sbin/vconfig add eth4 12
-/sbin/ifconfig eth3 172.16.201.1 netmask 255.255.255.0
-/sbin/route add -net 10.216.40.0 netmask 255.255.255.0 gw 172.16.201.254
-/sbin/route add -net 10.216.23.0 netmask 255.255.255.0 gw 172.16.201.254
-/sbin/route add -net 10.216.22.0 netmask 255.255.255.0 gw 172.16.201.254
+#/sbin/ifconfig eth3 172.16.201.1 netmask 255.255.255.0
+#/sbin/route add -net 10.216.40.0 netmask 255.255.255.0 gw 172.16.201.254
+#/sbin/route add -net 10.216.23.0 netmask 255.255.255.0 gw 172.16.201.254
+#/sbin/route add -net 10.216.22.0 netmask 255.255.255.0 gw 172.16.201.254
--- start_network_stuff2.sh
+++ start_network_stuff2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/sbin/openvpn --config /etc/openvpn/UCL-ovpn-client-scnd.conf 12> /home/connet/UCLvpn.log &
-/usr/sbin/openvpn --config /etc/openvpn/ofelia.conf 12> /home/connet/ofelia.log &
+/usr/sbin/openvpn --config /etc/openvpn/UCL-ovpn-client-scnd.conf  &
+/usr/sbin/openvpn --config /etc/openvpn/ofelia.conf &
 
