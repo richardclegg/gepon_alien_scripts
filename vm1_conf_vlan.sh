f356117b12eb3a07ea7560c4d972b2a849b50365
Richard G. Clegg <richard@richardclegg.org>
2014-08-20 00:34:34 +0100

    Tweeak scripts

--- vm1_conf_vlan.sh
+++ vm1_conf_vlan.sh
@@ -2,9 +2,14 @@
 
 modprobe 8021q
 vconfig add eth1 70
-sudo ifconfig eth1 up
-sudo ifconfig eth1.70 up
-route add 10.216.22.72 eth1.70
-route add 10.216.22.75 eth1.70
-route add 10.216.22.73 eth1.70
-route add 10.216.22.51 eth1.70
+ifconfig eth1 up
+if [ "$HOSTNAME" = tdur1 ]; then
+    ifconfig eth1.70 10.10.10.72/24 up
+elif [ "$HOSTNAME" = testit ]; then 
+    ifconfig eth1.70 10.10.10.75/24 up
+elif [ "$HOSTNAME" = tdel1 ]; then 
+    ifconfig eth1.70 10.10.10.73/24 up
+elif [ "$HOSTNAME" = ucl1 ]; then 
+    ifconfig eth1.70 10.10.10.51/24 up
+fi
+
