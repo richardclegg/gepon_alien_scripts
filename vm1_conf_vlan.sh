37e12101004ec175f5115edfff627a9f6423ea19
Richard G. Clegg <richard@richardclegg.org>
2014-08-20 00:39:41 +0100

    Update script

--- vm1_conf_vlan.sh
+++ vm1_conf_vlan.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 modprobe 8021q
 vconfig add eth1 70
 ifconfig eth1 up
-if [ "$HOSTNAME" = tdur1 ]; then
+if [ "$HOSTNAME" = "tdur1" ]; then
     ifconfig eth1.70 10.10.10.72/24 up
     echo "set tdur IP"
-elif [ "$HOSTNAME" = testit ]; then 
+elif [ "$HOSTNAME" = "testit" ]; then 
     ifconfig eth1.70 10.10.10.75/24 up
     echo "set testit IP"
-elif [ "$HOSTNAME" = tdel1 ]; then 
+elif [ "$HOSTNAME" = "tdel1" ]; then 
     ifconfig eth1.70 10.10.10.73/24 up
     echo "set tdel IP"
-elif [ "$HOSTNAME" = ucl1 ]; then 
+elif [ "$HOSTNAME" = "ucl1" ]; then 
     ifconfig eth1.70 10.10.10.51/24 up
 fi
 
