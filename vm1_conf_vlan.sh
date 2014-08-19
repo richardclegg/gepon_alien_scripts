4c508211fa9d02c6202e530398098f6755f24c1d
Richard G. Clegg <richard@richardclegg.org>
2014-08-20 00:36:29 +0100

    Tweeak scripts

--- vm1_conf_vlan.sh
+++ vm1_conf_vlan.sh
@@ -5,10 +5,13 @@ vconfig add eth1 70
 ifconfig eth1 up
 if [ "$HOSTNAME" = tdur1 ]; then
     ifconfig eth1.70 10.10.10.72/24 up
+    echo "set tdur IP"
 elif [ "$HOSTNAME" = testit ]; then 
     ifconfig eth1.70 10.10.10.75/24 up
+    echo "set testit IP"
 elif [ "$HOSTNAME" = tdel1 ]; then 
     ifconfig eth1.70 10.10.10.73/24 up
+    echo "set tdel IP"
 elif [ "$HOSTNAME" = ucl1 ]; then 
     ifconfig eth1.70 10.10.10.51/24 up
 fi
