1fe7a79cc41a74521bc6ff830aa46ba6cd973212
Richard G. Clegg <richard@richardclegg.org>
2014-08-19 23:57:38 +0100

    Tweeak scripts

--- vm1_conf_vlan.sh
+++ vm1_conf_vlan.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 modprobe 8021q
-vconfig add eth0 70
-sudo ifconfig eth0.70 up
-route add 10.216.22.72 eth0.70
-route add 10.216.22.75 eth0.70
-route add 10.216.22.73 eth0.70
-route add 10.216.22.51 eth0.70
+vconfig add eth1 70
+sudo ifconfig eth1.70 up
+route add 10.216.22.72 eth1.70
+route add 10.216.22.75 eth1.70
+route add 10.216.22.73 eth1.70
+route add 10.216.22.51 eth1.70
