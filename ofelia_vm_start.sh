71adfe1ef302c4406bc4cec360222a9f39dc8e1f
Richard G. Clegg <richard@richardclegg.org>
2014-08-21 00:15:57 +0100

    Fix script

--- ofelia_vm_start.sh
+++ ofelia_vm_start.sh
@@ -7,13 +7,13 @@ if [ "$HOSTNAME" = "tdur1" ]; then
     ifconfig eth1 10.43.21.1/24 up
     echo "set tdur IP"
 elif [ "$HOSTNAME" = "tdel1" ]; then 
-    ifconfig eth1.70 10.43.21.2/24 up
+    ifconfig eth1 10.43.21.2/24 up
     echo "set tdel IP"
 elif [ "$HOSTNAME" = "ucl1" ]; then 
-    ifconfig eth1.70 10.43.21.3/24 up
+    ifconfig eth1 10.43.21.3/24 up
     echo "set ucl1 IP"
 elif [ "$HOSTNAME" = "testit" ]; then 
-    ifconfig eth1.70 10.43.21.4/24 up
+    ifconfig eth1 10.43.21.4/24 up
     echo "set testit IP"
 else 
     echo "Host not found"
